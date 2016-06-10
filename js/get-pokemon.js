a5391396445ab4b58cb5d5003f6cea3d8811576f
sogo <sogoakinyosoye@hotmail.com>
2016-06-10 14:39:24 -0500

    initialized git repo

---
+++ js/get-pokemon.js
@@ -0,0 +1,43 @@
+var url = 'https://pokeapi.co/api/v2/ability/?limit=10&offset=20'
+
+var sogo= 'https://www.google.com'
+
+var template = $('.template')
+  .clone()
+  .removeClass('template')
+  .detach();
+
+function loadPokemon(pokemon) {
+    $.each(pokemon.results, function(i, pokemon) {
+    addPokemon(pokemon)
+    $.each(pokemon.pokemon, function(i, entry){
+  console.log(entry.pokemon.name);
+});
+
+
+
+
+
+
+  });
+}
+
+function addPokemon(pokemon) {
+  var li = template.clone();
+
+  li.find('.pokemon-name a')
+    .text(pokemon.name)
+    .attr('href', pokemon.url)
+    .filter("#name")
+
+  li.attr('data-name', pokemon.id);
+  $('#pokemonList').append(li);
+
+
+}
+
+
+$.get({
+  url: url,
+  success: loadPokemon,
+});
