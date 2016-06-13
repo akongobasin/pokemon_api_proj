c8b5646fad9055cb3b4622124f7d2c609ed61a84
mketiku <mketiku@gmail.com>
2016-06-12 23:51:27 -0400

    Adding scripts for jquery and eventful

--- js/get-pokemon.js
+++ js/get-pokemon.js
@@ -1,36 +1,37 @@
-//var url = 'https://pokeapi.co/api/v2/pokemon/'
+var url = 'https://pokeapi.co/api/v2/pokemon/'
 //
 
-var template = $('.template')
-  .clone()
-  .removeClass('template')
-  .detach();
 
-function loadPokemon(pokemon) {
-      $.each(pokemon.results, function(i, pokemon) {
-          //addPokemon(pokemon.name);
-          console.log(pokemon.name);
+var shows = {
+    search: function(cb){
+        $.getJSON('http://api.eventful.com/json/events/search?c=music&app_key=jcP6d2QX9tSJ6CDg&keywords=childish+gambino&callback=?', cb);
+        $.getJSON('htpp://api.eventful.com/json/events/search?&app_key=jcP6d2QX9tSJ6CDg&location=indianapolis&t=Today&within=10miles&callback=?')
+};
 
-          //logs pokemon name results in the console
-  });
+
+var 
+function addDate(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+        dd='0'+dd
+    }
+
+    if(mm<10) {
+        mm='0'+mm
+    }
+
+    today = mm+'/'+dd+'/'+yyyy;
+   return (console.log(today));
 }
 
-function addPokemon(pokemon) {
-  var li = template.clone();
 
-  li.find('.pokemon-name a')
-    .text(pokemon.name)
-    .attr('href', pokemon.url)
-    .filter("#name")
-
-  li.attr('data-name', pokemon.id);
-  $('#pokemonList').append(li);
-
-
-}
 
 
 $.get({
   url: url,
-  success: loadPokemon
+  success: addDate
 });
